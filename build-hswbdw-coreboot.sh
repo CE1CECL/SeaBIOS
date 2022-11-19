ec0a0175b4c35c8a8baf3075103b72a6f8137c3a
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2022-11-19 15:19:16 -0500

    c

--- build-hswbdw-coreboot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-set -e
-rm -rf ./out
-cp configs/.config-hswbdw-coreboot .config
-make EXTRAVERSION=-MrChromebox-`date +"%Y.%m.%d"`
-cp ./out/bios.bin.elf ../coreboot/seabios-hswbdw.bin.elf
