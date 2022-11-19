7ce7d51aced371fb1c5e05cfa9cd0f3a2bd8b8cc
ChrisEric1 CECL <christopherericlentocha@gmail.com>
2022-11-19 15:09:05 -0500

    b

--- build-byt-coreboot.sh
+++ build-byt-coreboot.sh
@@ -3,7 +3,6 @@
 set -e
 rm -rf ./out
 cp configs/.config-byt-coreboot .config
-make olddefconfig
 make EXTRAVERSION=-MrChromebox-`date +"%Y.%m.%d"`
 cp ./out/bios.bin.elf ../coreboot/seabios-byt.bin.elf
 filename="seabios-byt_bootstub-mrchromebox_`date +"%Y%m%d"`.bin"
